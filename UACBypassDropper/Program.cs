3a542afe2b70f27a2258ece6151a18770cc507a0
0x0000000A <coderx927+22@gmail.com>
2025-04-26 23:24:48 +0100

    Add files via upload

---
+++ UACBypassDropper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UACBypassDropper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[+] Starting!");
+            try
+            {
+                Dropper.Execute();
+                Console.WriteLine("[+] Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[-] Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ UACBypassDropper/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UACBypassDropper")]
+[assembly: AssemblyDescription("Unsafe UAC Bypass Dropper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UACBypassDropper")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("D6AFCB65-9D89-4F2C-B676-98B68D3B8031")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
